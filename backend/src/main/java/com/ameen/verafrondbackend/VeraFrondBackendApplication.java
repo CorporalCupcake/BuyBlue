01777292509e24024cbbaa051d0d6bc046a0489c
CorporalCupcake <ayubameen1@gmail.com>
2022-03-17 12:10:34 +0400

    Started Spring Backend

---
+++ backend/src/main/java/com/ameen/verafrondbackend/VeraFrondBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ameen.verafrondbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeraFrondBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeraFrondBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/ameen/verafrondbackend/VeraFrondBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ameen.verafrondbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeraFrondBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
